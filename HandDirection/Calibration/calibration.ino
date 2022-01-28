8adb935cc0d9145e9528ec8fa5c87c6a18c4d6e3
florian6973 <70778912+florian6973@users.noreply.github.com>
2022-01-28 15:43:05 +0100

    MAJ to BLE

--- HandDirection/Calibration/calibration.ino
+++ HandDirection/Calibration/calibration.ino
@@ -25,15 +25,11 @@
   *****************************************************************/
   // The SFE_LSM9DS1 library requires both Wire and SPI be
   // included BEFORE including the 9DS1 library.
-  #include <Wire.h>
-  #include <SPI.h>
-  #include <SparkFunLSM9DS1.h>
-  
+#include <Arduino_LSM9DS1.h>
   //////////////////////////
   // LSM9DS1 Library Init //
   //////////////////////////
   // default settings gyro  245 d/s, accel = 2g, mag = 4G
-  LSM9DS1 imu;
   
   ///////////////////////
   // Example I2C Setup //
@@ -54,25 +50,24 @@
     Serial.begin(9600);
     while (!Serial); //wait for connection
   
-    Wire.begin();
+   
+  while (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    delay (100);
+  }
   
-    if (imu.begin() == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
-    {
-      Serial.println(F("LSM9DS1 not detected"));
-      while (1);
-    }
     Serial.println("Collecting gyro data, hold still");
     delay(3000);
     // get gyro offset
     long gxa = 0, gya = 0, gza = 0;
     for (int i = 0; i < 300; i++) {
-      if ( imu.gyroAvailable() )
-      {
-        imu.readGyro();
-        gxa += imu.gx;
-        gya += imu.gy;
-        gza += imu.gz;
-      }
+      
+        float gx, gy, gz;
+        IMU.readGyroscope(gx, gy, gz);
+        gxa += gx;
+        gya += gy;
+        gza += gz;
+      
     }
     Serial.println(F("gyro offsets"));
     Serial.print(gxa / 300);
@@ -91,32 +86,24 @@
   {
     static int n = 0; //count values transmitted
     // Update the sensor values whenever new data is available
-  
-    if ( imu.accelAvailable() ) 
-    {
-      // To read from the accelerometer, first call the
-      // readAccel() function. When it exits, it'll update the
-      // ax, ay, and az variables with the most current data.
-      imu.readAccel();
-    }
-    if ( imu.magAvailable() )
-    {
-      imu.readMag();
-    }
+
+  float aax, aay, aaz, mmx, mmy, mmz;  
+    IMU.readAcceleration(aax, aay, aaz);
+    IMU.readMagneticField(mmx, mmy, mmz);
   
     if (millis() - lastPrint > PRINT_SPEED)
     {
-      Serial.print(imu.ax);
+      Serial.print(aax);
       Serial.print(", ");
-      Serial.print(imu.ay);
+      Serial.print(aay);
       Serial.print(", ");
-      Serial.print(imu.az);
+      Serial.print(aaz);
       Serial.print(", ");
-      Serial.print(imu.mx);
+      Serial.print(mmx);
       Serial.print(", ");
-      Serial.print(imu.my);
+      Serial.print(mmy);
       Serial.print(", ");
-      Serial.println(imu.mz);
+      Serial.println(mmz);
       if (n++ > 299) {
         Serial.println("Done.");
         while (1); //hang here
